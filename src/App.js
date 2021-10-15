edb6d06fa35fca41ffc489b0a1b3e2e404c8d958
juanrivera-cs <juan.rivera@colimasoft.com>
2021-10-15 13:01:38 -0500

    add env dev

--- src/App.js
+++ src/App.js
@@ -8,6 +8,7 @@ function App() {
         <img src={logo} className="App-logo" alt="logo" />
         <p>
           Edit <code>src/App.js</code> and save to reload.
+          Your enviroment is  {process.env.REACT_APP_BASE_URL}
         </p>
         <a
           className="App-link"
