bb3696b1d5047d1ed791479a4e5bbaaed3883b55
juanrivera-cs <juan.rivera@colimasoft.com>
2021-10-26 16:13:32 -0500

    add api to endpoint

--- src/apis/config.js
+++ src/apis/config.js
@@ -1,3 +1,3 @@
 //export const baseURL = 'http://localhost:3977/api';
-export const baseURL = 'https://app-maximaderas.herokuapp.com';
+export const baseURL = 'https://app-maximaderas.herokuapp.com/api';
 export const headers = {"mode":"no-cors", "headers": {"Access-Control-Allow-Origin":"*"}}
