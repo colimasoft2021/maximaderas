b0c48744b505da466a592f7da41fe2941ca466bb
juanrivera-cs <juan.rivera@colimasoft.com>
2021-11-01 13:57:14 -0600

    Updates

---
+++ precache-manifest.046ffd0f2f2444274dd260c33d0ed28c.js
@@ -0,0 +1,218 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6a8ff14638e789de4ac8f0088ce02a89",
+    "url": "/maximaderas/index.html"
+  },
+  {
+    "revision": "a8d1cddfcd9206f82130",
+    "url": "/maximaderas/static/css/22.5bd729c9.chunk.css"
+  },
+  {
+    "revision": "b9a58ea446c4e2850120",
+    "url": "/maximaderas/static/css/23.5bd729c9.chunk.css"
+  },
+  {
+    "revision": "9c48acfa1e255a51e41a",
+    "url": "/maximaderas/static/css/9.daa307f6.chunk.css"
+  },
+  {
+    "revision": "24553a137d98eff034f9",
+    "url": "/maximaderas/static/css/main.772cbfe5.chunk.css"
+  },
+  {
+    "revision": "02c254ff7f48502bb728",
+    "url": "/maximaderas/static/js/0.7686696d.chunk.js"
+  },
+  {
+    "revision": "b5f1e98ab70b6df88df2",
+    "url": "/maximaderas/static/js/1.41983995.chunk.js"
+  },
+  {
+    "revision": "c9f0fcfa80f53de48cfb",
+    "url": "/maximaderas/static/js/10.36d005ef.chunk.js"
+  },
+  {
+    "revision": "45a6bef00b472c2a68fc",
+    "url": "/maximaderas/static/js/11.3c1756a7.chunk.js"
+  },
+  {
+    "revision": "e9b17d8434880e07ca29",
+    "url": "/maximaderas/static/js/12.ff36fab0.chunk.js"
+  },
+  {
+    "revision": "bdff62aae8eb84ad20dd",
+    "url": "/maximaderas/static/js/13.7d086a04.chunk.js"
+  },
+  {
+    "revision": "5fd082fa25ec1dab3e67",
+    "url": "/maximaderas/static/js/14.fb54e436.chunk.js"
+  },
+  {
+    "revision": "045d849a3cce7445ae5e",
+    "url": "/maximaderas/static/js/15.1f71b637.chunk.js"
+  },
+  {
+    "revision": "716080ef820fb48718b7",
+    "url": "/maximaderas/static/js/16.dad02bc4.chunk.js"
+  },
+  {
+    "revision": "d00ae7c85f36d8e6da38",
+    "url": "/maximaderas/static/js/17.9c614890.chunk.js"
+  },
+  {
+    "revision": "9509b9a86e3d54d817a3",
+    "url": "/maximaderas/static/js/18.b2726012.chunk.js"
+  },
+  {
+    "revision": "633dd44df2b27c604eb4",
+    "url": "/maximaderas/static/js/19.f6a3e78d.chunk.js"
+  },
+  {
+    "revision": "5b8eb09bf43b60b4c66a",
+    "url": "/maximaderas/static/js/2.d7252a22.chunk.js"
+  },
+  {
+    "revision": "a8f62ce26d1b36275128",
+    "url": "/maximaderas/static/js/20.e624bf81.chunk.js"
+  },
+  {
+    "revision": "c1db8e2b8192901498ef",
+    "url": "/maximaderas/static/js/21.dbe26b4f.chunk.js"
+  },
+  {
+    "revision": "a8d1cddfcd9206f82130",
+    "url": "/maximaderas/static/js/22.4b4a90d5.chunk.js"
+  },
+  {
+    "revision": "b9a58ea446c4e2850120",
+    "url": "/maximaderas/static/js/23.7afe32a3.chunk.js"
+  },
+  {
+    "revision": "dcd1b0c3808f5badc26e",
+    "url": "/maximaderas/static/js/24.79bd179c.chunk.js"
+  },
+  {
+    "revision": "ceea93db17aba288cba3",
+    "url": "/maximaderas/static/js/25.d0a62a77.chunk.js"
+  },
+  {
+    "revision": "eb8c700a8f55e78209a6",
+    "url": "/maximaderas/static/js/26.04a2c018.chunk.js"
+  },
+  {
+    "revision": "baf82012c4c6eaa0d1b8",
+    "url": "/maximaderas/static/js/27.bbe3a4f7.chunk.js"
+  },
+  {
+    "revision": "a2fd35a7c838fc7d374b",
+    "url": "/maximaderas/static/js/28.0adaa5ac.chunk.js"
+  },
+  {
+    "revision": "2f79a014a7cfd056914b",
+    "url": "/maximaderas/static/js/29.aea0c167.chunk.js"
+  },
+  {
+    "revision": "2b382934681904deab88",
+    "url": "/maximaderas/static/js/3.d98e24b4.chunk.js"
+  },
+  {
+    "revision": "f26017e5fc59d7455115",
+    "url": "/maximaderas/static/js/30.8d414dc0.chunk.js"
+  },
+  {
+    "revision": "6b363a5850beb1fa75d9",
+    "url": "/maximaderas/static/js/31.4dc125a1.chunk.js"
+  },
+  {
+    "revision": "4cacaf0548ab909610a0",
+    "url": "/maximaderas/static/js/32.690de96a.chunk.js"
+  },
+  {
+    "revision": "3968068bbbef5b49be8b",
+    "url": "/maximaderas/static/js/33.7dfb2dfd.chunk.js"
+  },
+  {
+    "revision": "748cfc93fdba0564fee0",
+    "url": "/maximaderas/static/js/34.bddfe12e.chunk.js"
+  },
+  {
+    "revision": "304f555a8c63ca4d6a6b",
+    "url": "/maximaderas/static/js/35.d8526c9f.chunk.js"
+  },
+  {
+    "revision": "368d779c562a3ca78361",
+    "url": "/maximaderas/static/js/36.303a8016.chunk.js"
+  },
+  {
+    "revision": "e9000dd6aa6d70167c26",
+    "url": "/maximaderas/static/js/4.8877621b.chunk.js"
+  },
+  {
+    "revision": "579c74ee54a2d0087338",
+    "url": "/maximaderas/static/js/5.bbb8987c.chunk.js"
+  },
+  {
+    "revision": "b45f11bbaee55baa757c",
+    "url": "/maximaderas/static/js/6.310deedf.chunk.js"
+  },
+  {
+    "revision": "9c48acfa1e255a51e41a",
+    "url": "/maximaderas/static/js/9.1c88bfa2.chunk.js"
+  },
+  {
+    "revision": "24553a137d98eff034f9",
+    "url": "/maximaderas/static/js/main.25bc9ce0.chunk.js"
+  },
+  {
+    "revision": "893553e6d34b794e81ab",
+    "url": "/maximaderas/static/js/runtime-main.3323f2bf.js"
+  },
+  {
+    "revision": "01798bc13e33afc36a52f2826638d386",
+    "url": "/maximaderas/static/media/Pe-icon-7-stroke.01798bc1.ttf"
+  },
+  {
+    "revision": "71394c0c7ad6c1e7d5c77e8ac292fba5",
+    "url": "/maximaderas/static/media/Pe-icon-7-stroke.71394c0c.eot"
+  },
+  {
+    "revision": "b38ef310874bdd008ac14ef3db939032",
+    "url": "/maximaderas/static/media/Pe-icon-7-stroke.b38ef310.woff"
+  },
+  {
+    "revision": "dedf26989fdd78c33cb9ae664a604d6c",
+    "url": "/maximaderas/static/media/Pe-icon-7-stroke.dedf2698.svg"
+  },
+  {
+    "revision": "32400f4e08932a94d8bfd2422702c446",
+    "url": "/maximaderas/static/media/fontawesome-webfont.32400f4e.eot"
+  },
+  {
+    "revision": "65e8f7dd6a65aa5812afa9050bfc720e",
+    "url": "/maximaderas/static/media/fontawesome-webfont.65e8f7dd.svg"
+  },
+  {
+    "revision": "a35720c2fed2c7f043bc7e4ffb45e073",
+    "url": "/maximaderas/static/media/fontawesome-webfont.a35720c2.woff"
+  },
+  {
+    "revision": "a3de2170e4e9df77161ea5d3f31b2668",
+    "url": "/maximaderas/static/media/fontawesome-webfont.a3de2170.ttf"
+  },
+  {
+    "revision": "db812d8a70a4e88e888744c1c9a27e89",
+    "url": "/maximaderas/static/media/fontawesome-webfont.db812d8a.woff2"
+  },
+  {
+    "revision": "1fbfd4bcffccb94e8e8a5ea70616b296",
+    "url": "/maximaderas/static/media/lg.1fbfd4bc.woff"
+  },
+  {
+    "revision": "2ec2cb2199d4d881e6a6ad86690f6add",
+    "url": "/maximaderas/static/media/lg.2ec2cb21.svg"
+  },
+  {
+    "revision": "f4292655f93dd12d9b8e4fc067ef2489",
+    "url": "/maximaderas/static/media/lg.f4292655.ttf"
+  }
+]);
